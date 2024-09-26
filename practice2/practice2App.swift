d7d03008e697e2be5981cb26084c353953e802c5
balshammri04 <bayanalshammri7@gmail.com>
2024-09-26 17:02:43 +0300

    Initial Commit

---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by bayan alshammri on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
